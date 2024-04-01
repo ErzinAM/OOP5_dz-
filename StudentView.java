624ec886adbed5ea47ceebed2385a5b8fb9cef40
ErzinAM <150605651+ErzinAM@users.noreply.github.com>
2024-04-01 21:23:30 +0300

    StudentView.java

---
+++ StudentView.java
@@ -0,0 +1,8 @@
+public class StudentView implements UserView<Student> {
+    @Override
+    public void sendOnConsole(List<Student> list) {
+        for (Student student : list) {
+            System.out.println(student);
+        }
+    }
+}
